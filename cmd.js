cad9eb846bbaad8dcb864cc6508c0949994951bc
Sean West <sean.e.west@gmail.com>
2014-12-23 02:35:40 -0500

    file name inconsistency

--- cmd.js
+++ cmd.js
@@ -3,6 +3,6 @@
 var atom = require('atom-shell')
 var proc = require('child_process')
 var args = process.argv.slice(2);
-var atom_entry = __dirname + "/index.js";
+var atom_entry = __dirname + "/main.js";
 var atom_args = [atom_entry].concat(args);
 var child = proc.spawn(atom, atom_args);
